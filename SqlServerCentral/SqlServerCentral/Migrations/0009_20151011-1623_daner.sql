9789892f7d118ad78d3d28306d9c4dc87dfad094
Bob <bob@bobbirch.com>
2015-10-11 16:23:57 +1100

    UpdatedDate col

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0009_20151011-1623_daner.sql
@@ -0,0 +1,10 @@
+﻿-- <Migration ID="6f027578-cea6-422a-a7e9-18fae9bc15c0" />
+GO
+
+PRINT N'Altering [dbo].[StaticTextFragments]...';
+
+
+GO
+ALTER TABLE [dbo].[StaticTextFragments]
+    ADD [UpdatedDate] DATETIME2 (7) CONSTRAINT [DF_StaticTextFragments_UpdatedDate] DEFAULT sysdatetime() NOT NULL;
+
--- SqlServerCentral/SqlServerCentral/Schema-Model/Tables/dbo.StaticTextFragments.sql
+++ SqlServerCentral/SqlServerCentral/Schema-Model/Tables/dbo.StaticTextFragments.sql
@@ -4,8 +4,9 @@ CREATE TABLE [dbo].[StaticTextFragments]
 [KeyText] [varchar] (50) NOT NULL,
 [StaticText] [varchar] (8000) NULL,
 [ContainsTokens] [bit] NOT NULL,
+[PublishDate] [datetime2] NOT NULL,
 [CreatedDate] [datetime2] NULL CONSTRAINT [DF_StaticTextFragments_CreatedDate] DEFAULT (sysdatetime()),
-[PublishDate] [datetime2] NOT NULL
+[UpdatedDate] [datetime2] NOT NULL CONSTRAINT [DF_StaticTextFragments_UpdatedDate] DEFAULT (sysdatetime())
 )
 GO
 ALTER TABLE [dbo].[StaticTextFragments] ADD CONSTRAINT [PK_StaticTextFragments] PRIMARY KEY CLUSTERED  ([StaticTextFragmentID])
