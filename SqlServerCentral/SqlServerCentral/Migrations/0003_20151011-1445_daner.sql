596a56532c99147aeab1c8f64cab0c81f6477bc4
Daniel Nolan <daniel@ready-roll.com>
2015-10-11 14:46:12 +1100

    Changed proc

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0003_20151011-1445_daner.sql
@@ -0,0 +1,19 @@
+﻿-- <Migration ID="991207e6-747b-4a4b-87f7-fc0416dd0ab0" />
+GO
+
+
+PRINT N'Altering [dbo].[AddStopWord]...';
+
+
+GO
+ALTER PROCEDURE dbo.AddStopWord @Word NVARCHAR(50)
+AS
+	BEGIN
+		INSERT INTO dbo.SearchAnalyzerConfig
+		        ( Word, [Group] )
+		VALUES  ( @Word, 1 )
+		
+		
+		SELECT @@ERROR AS 'atatError'
+	END
+GO
