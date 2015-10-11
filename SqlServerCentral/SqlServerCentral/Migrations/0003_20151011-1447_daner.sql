77759a427db7450fbd9245611cd16d2801bb5ef5
Daniel Nolan <daniel@ready-roll.com>
2015-10-11 14:50:34 +1100

    Changed proc precision

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0003_20151011-1447_daner.sql
@@ -0,0 +1,21 @@
+﻿-- <Migration ID="e9720f33-ad07-4c05-b910-6c583fec57d3" />
+GO
+
+
+GO
+PRINT N'Altering [dbo].[AddStopWord]...';
+
+
+GO
+ALTER PROCEDURE [dbo].[AddStopWord] @Word NVARCHAR(60)
+AS
+	BEGIN
+		INSERT INTO dbo.SearchAnalyzerConfig
+		        ( Word, [Group] )
+		VALUES  ( @Word, 0 )
+		
+		
+		SELECT @@ERROR AS 'atatError'
+	END
+GO
+
