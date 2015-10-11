ba7da58408641164bcad8bc37f0f0c05caccf759
Daniel Nolan <daniel@ready-roll.com>
2015-10-11 15:16:45 +1100

    New col updatedate

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0005_20151011-1515_daner.sql
@@ -0,0 +1,10 @@
+﻿-- <Migration ID="7c126564-1fe1-41a4-b3cd-9055a080dbba" />
+GO
+
+PRINT N'Altering [dbo].[Articles]...';
+
+
+GO
+ALTER TABLE [dbo].[Articles]
+    ADD [UpdatedDate] DATETIME2 (7) CONSTRAINT [DF_Articles_UpdatedDate] DEFAULT sysdatetime() NOT NULL;
+
