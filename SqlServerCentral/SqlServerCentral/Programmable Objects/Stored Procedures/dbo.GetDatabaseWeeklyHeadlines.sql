3dc202fef963ff705b5676c22ed062a47a3a818b
Daniel Nolan <daniel@ready-roll.com>
2015-10-11 15:53:25 +1100

    Bob

--- SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.GetDatabaseWeeklyHeadlines.sql
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.GetDatabaseWeeklyHeadlines.sql
@@ -12,7 +12,7 @@ AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
 	-- interfering with SELECT statements.
-	SET NOCOUNT ON;
+	SET NOCOUNT OFF;
 
 	exec DatabaseWeekly.dbo.[sp_NewsletterGetIssue] @start=@fromDate
 END
