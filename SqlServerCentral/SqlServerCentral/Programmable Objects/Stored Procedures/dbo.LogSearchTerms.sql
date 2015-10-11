4c37d030e59098c1ad807e9b0b9d6729bebbb19d
Daniel Nolan <daniel@ready-roll.com>
2015-10-11 15:20:57 +1100

    Extracted code objects

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.LogSearchTerms.sql
@@ -0,0 +1,26 @@
+﻿IF OBJECT_ID('[dbo].[LogSearchTerms]') IS NOT NULL
+	DROP PROCEDURE [dbo].[LogSearchTerms];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[LogSearchTerms]
+@UserID INT = NULL,
+@Search NVARCHAR(1000)
+AS
+BEGIN
+
+INSERT INTO UserSearches(UserID, Search)
+	SELECT @UserID, @Search
+
+-- return an int for NHibernate - there may be a way to not require a return value...
+SELECT @@ERROR AS atatError
+
+END
+
+
+GO
+GRANT EXECUTE ON  [dbo].[LogSearchTerms] TO [ssc_webapplication]
+GO
---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.UpdatePopularityRanks.sql
@@ -0,0 +1,26 @@
+﻿IF OBJECT_ID('[dbo].[UpdatePopularityRanks]') IS NOT NULL
+	DROP PROCEDURE [dbo].[UpdatePopularityRanks];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[UpdatePopularityRanks]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	UPDATE dbo.ContentItems
+	SET PopularityRank = cpr.PopularityRank
+	FROM dbo.vw_CurrentContentPerformanceRecord cpr
+	WHERE ContentItems.ContentItemID = cpr.ContentItemID
+END
+GO
