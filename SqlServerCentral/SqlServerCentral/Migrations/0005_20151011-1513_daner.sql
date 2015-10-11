c0f11edc4990116d07f958c3f7fb88ecf12537b6
Daniel Nolan <daniel@ready-roll.com>
2015-10-11 15:14:32 +1100

    Added created date column

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0005_20151011-1513_daner.sql
@@ -0,0 +1,12 @@
+﻿-- <Migration ID="6ad2757c-a973-46dd-85a4-6a575ea18042" />
+GO
+
+PRINT N'Altering [dbo].[Answers]...';
+GO
+ALTER TABLE [dbo].[Answers]
+    ADD [CreatedDate] DATETIME2 (7)
+	CONSTRAINT [DF_Answers_CreatedDate] 
+	DEFAULT sysdatetime() NOT NULL;
+
+GO
+
