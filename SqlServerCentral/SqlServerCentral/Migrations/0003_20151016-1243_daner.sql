9e3036b4efaef12824ac563c440b94b41e2c7d9b
Bob <bob@bobslidell.com>
2015-10-16 12:45:23 +0100

    CreatedDate

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0003_20151016-1243_daner.sql
@@ -0,0 +1,27 @@
+﻿-- <Migration ID="3c1da6fe-5458-4802-b516-68c4d6c1a28f" />
+GO
+
+-- 0003_EmailerConfiguration_CreatedDate.sql
+
+PRINT N'Adding [dbo].[EmailerConfiguration].[CreatedDate] column (empty)'
+GO
+ALTER TABLE [dbo].[EmailerConfiguration] 
+	ADD [CreatedDate] [datetime2] NULL
+GO
+
+PRINT 'Populating existing rows with data from the [LastModifiedDate] column...'
+UPDATE dbo.EmailerConfiguration
+	SET CreatedDate = LastModifiedDate
+	WHERE CreatedDate IS NULL
+GO
+
+PRINT 'Adding default constraint...'
+ALTER TABLE dbo.EmailerConfiguration 
+	ADD CONSTRAINT DF_EmailerConfiguration_CreatedDate 
+	DEFAULT sysdatetime() 
+	FOR CreatedDate
+GO
+
+PRINT 'Done. Altering column to NOT NULL'
+ALTER TABLE [dbo].[EmailerConfiguration] ALTER COLUMN [CreatedDate] [datetime2] NOT NULL
+GO
