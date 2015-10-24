d01f6c6818e662a0d3fbb9c9380864a6e45e6a1d
Bob <bob@bobslidell.com>
2015-10-24 22:26:25 +0100

    Proc baseline

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.prcGetTodaysContent.sql
@@ -0,0 +1,21 @@
+﻿IF OBJECT_ID('[dbo].[prcGetTodaysContent]') IS NOT NULL
+	DROP PROCEDURE [dbo].[prcGetTodaysContent];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+--SET QUOTED_IDENTIFIER ON|OFF
+--SET ANSI_NULLS ON|OFF
+--GO
+
+CREATE PROCEDURE [dbo].[prcGetTodaysContent]
+	--@parameter_name AS scalar_data_type ( = default_value ), ...
+-- WITH ENCRYPTION, RECOMPILE, EXECUTE AS CALLER|SELF|OWNER| 'user_name'
+AS
+	
+	SELECT * FROM  dbo.ContentItems
+	WHERE CreatedDate = GETDATE() 
+	
+GO
