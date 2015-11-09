98672bbdb9c839de3e9246b18687b5515c0396f6
Bob <bob@bobslidell.com>
2015-11-09 21:25:09 +1100

    Added baseline

---
+++ SqlServerCentral/SqlServerCentral/Migrations/1.1.0-Feature1/0003_20151109-2123_daner.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="6809d412-fa1c-4820-b89f-afe8dfd30803" />
+GO
+
+PRINT N'Altering [dbo].[EmailerConfiguration]'
+GO
+ALTER TABLE [dbo].[EmailerConfiguration] ADD
+[CreatedDate] [datetime2] NOT NULL CONSTRAINT [DF_EmailerConfiguration_CreatedDate] DEFAULT (sysdatetime())
+GO
