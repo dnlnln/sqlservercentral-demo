aedb58bccf6a69039b0490063451cb73f73c20f8
Jim <Jim@jimmidean.com>
2015-11-11 21:25:07 +1100

    Baseline procs

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.User_BanUser.sql
@@ -0,0 +1,24 @@
+﻿IF OBJECT_ID('[dbo].[User_BanUser]') IS NOT NULL
+	DROP PROCEDURE [dbo].[User_BanUser];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[User_BanUser] (@Username VARCHAR(200))
+AS
+BEGIN
+	IF NOT EXISTS (SELECT * 
+					FROM SqlServerCentralForums.dbo.BannedUsers
+					WHERE username = @Username)
+	BEGIN
+		INSERT INTO SqlServerCentralForums.dbo.BannedUsers
+				( username )
+		VALUES  ( @Username )
+        SELECT @Username AS username
+	END
+END
+GO
+GRANT EXECUTE ON  [dbo].[User_BanUser] TO [ssc_webapplication]
+GO
