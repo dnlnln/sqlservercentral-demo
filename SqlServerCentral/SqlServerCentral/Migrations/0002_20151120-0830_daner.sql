9d0c5ccfb916e5976260859fc83594e3a44450c4
Bob <bob@bobslidell.com>
2015-11-20 08:32:11 +1100

    Baseline

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0002_20151120-0830_daner.sql
@@ -0,0 +1,27 @@
+﻿-- <Migration ID="291e6fcf-6eb6-4a5b-bb72-6248789fb024" />
+GO
+
+SET DATEFORMAT YMD;
+
+
+GO
+DECLARE @pv AS BINARY (16);
+
+
+GO
+IF (SELECT COUNT(*)
+    FROM   [dbo].[EmailerConfiguration]) = 0
+    BEGIN
+        PRINT (N'Add 4 rows to [dbo].[EmailerConfiguration]');
+        INSERT  INTO [dbo].[EmailerConfiguration] ([ComputerName], [Active], [BatchSize], [Priority], [LastModifiedDate])
+        VALUES                                   (N'252314-SQL1', 1, 100, 1, '2014-12-01 12:48:12.0000000');
+        INSERT  INTO [dbo].[EmailerConfiguration] ([ComputerName], [Active], [BatchSize], [Priority], [LastModifiedDate])
+        VALUES                                   (N'252315-SQL2', 1, 1000, 2, '2014-12-01 12:48:12.0000000');
+        INSERT  INTO [dbo].[EmailerConfiguration] ([ComputerName], [Active], [BatchSize], [Priority], [LastModifiedDate])
+        VALUES                                   (N'252838-VMWEB3', 1, 10, 1, '2014-12-01 12:48:12.0000000');
+        INSERT  INTO [dbo].[EmailerConfiguration] ([ComputerName], [Active], [BatchSize], [Priority], [LastModifiedDate])
+        VALUES                                   (N'252839-VMWEB4', 1, 1000, 2, '2014-12-01 12:48:12.0000000');
+    END
+
+
+GO
