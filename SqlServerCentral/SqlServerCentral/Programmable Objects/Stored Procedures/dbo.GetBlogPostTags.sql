8932063dfd381a97e34d708b130e27acbe446abc
Bob <bob@bobslidell.com>
2016-01-14 19:48:07 +1100

    Baseline

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.GetBlogPostTags.sql
@@ -0,0 +1,21 @@
+﻿IF OBJECT_ID('[dbo].[GetBlogPostTags]') IS NOT NULL
+	DROP PROCEDURE [dbo].[GetBlogPostTags];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE PROC [dbo].[GetBlogPostTags]
+	@PostID INT
+AS
+	SELECT Name AS TagName
+	FROM CommunityServer.dbo.cs_Posts_InCategories
+	INNER JOIN CommunityServer.dbo.cs_Post_Categories
+		ON CommunityServer.dbo.cs_Posts_InCategories.CategoryID = CommunityServer.dbo.cs_Post_Categories.CategoryID
+		AND CommunityServer.dbo.cs_Posts_InCategories.SettingsID = CommunityServer.dbo.cs_Post_Categories.SettingsID
+	WHERE PostID = @PostID
+GO
+GRANT EXECUTE ON  [dbo].[GetBlogPostTags] TO [ssc_webapplication]
+GO
