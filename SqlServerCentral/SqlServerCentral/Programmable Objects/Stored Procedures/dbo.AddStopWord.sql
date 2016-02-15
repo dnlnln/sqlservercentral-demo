f7294dd5ae27b6b8341f97fd8d63c36f1de68dc1
Bob <bob@bobslidell.com>
2016-02-15 13:18:25 +0000

    Added programmable objects

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.AddStopWord.sql
@@ -0,0 +1,23 @@
+IF OBJECT_ID('[dbo].[AddStopWord]') IS NOT NULL
+	DROP PROCEDURE [dbo].[AddStopWord];
+
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[AddStopWord] @Word NVARCHAR(50)
+AS
+	BEGIN
+		INSERT INTO dbo.SearchAnalyzerConfig
+		        ( Word, [Group] )
+		VALUES  ( @Word, 1 )
+		
+		
+		SELECT @@ERROR AS 'atatError'
+	END
+GO
+
+GRANT EXECUTE ON  [dbo].[AddStopWord] TO [ssc_webapplication]
+GO
