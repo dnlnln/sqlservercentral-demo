c4053ccc0007e2d02b3ad5464b2c2995399c93dc
Bob <bob@bobslidell.com>
2016-03-09 18:41:06 +1100

    Initial commit

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.Email_GetMailboxMonitorConfiguration.sql
@@ -0,0 +1,22 @@
+﻿IF OBJECT_ID('[dbo].[Email_GetMailboxMonitorConfiguration]') IS NOT NULL
+	DROP PROCEDURE [dbo].[Email_GetMailboxMonitorConfiguration];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE proc [dbo].[Email_GetMailboxMonitorConfiguration]
+AS  
+    SELECT  ID,
+            MailboxAddress,
+            MailboxPassword,
+            MailServer,
+            AccountType
+    FROM    dbo.MailboxMonitorConfiguration
+    ORDER BY AccountType DESC 
+
+GO
+GRANT EXECUTE ON  [dbo].[Email_GetMailboxMonitorConfiguration] TO [Emailer]
+GRANT EXECUTE ON  [dbo].[Email_GetMailboxMonitorConfiguration] TO [ssc_emailer]
+GO
---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/Stored Procedures/dbo.ListStopWords.sql
@@ -0,0 +1,19 @@
+﻿IF OBJECT_ID('[dbo].[ListStopWords]') IS NOT NULL
+	DROP PROCEDURE [dbo].[ListStopWords];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[ListStopWords]
+AS
+    BEGIN
+        SELECT  Word
+        FROM    dbo.SearchAnalyzerConfig
+        WHERE   [Group] = 0
+    END
+GO
+GRANT EXECUTE ON  [dbo].[ListStopWords] TO [ssc_searchindexer]
+GRANT EXECUTE ON  [dbo].[ListStopWords] TO [ssc_webapplication]
+GO
