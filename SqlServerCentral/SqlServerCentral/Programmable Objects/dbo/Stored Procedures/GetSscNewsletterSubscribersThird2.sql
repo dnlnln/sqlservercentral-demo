fb41fa0157d362d5aa4ca241bb8b93e442313b75
Bob <bob@bobslidell.com>
2016-03-11 07:45:20 +1100

    Added programmable objects

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/dbo/Stored Procedures/GetSscNewsletterSubscribersThird2.sql
@@ -0,0 +1,21 @@
+﻿IF OBJECT_ID('[dbo].[GetSscNewsletterSubscribersThird2]') IS NOT NULL
+	DROP PROCEDURE [dbo].[GetSscNewsletterSubscribersThird2];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+
+CREATE PROCEDURE [dbo].[GetSscNewsletterSubscribersThird2]
+AS BEGIN
+	SELECT DISTINCT UserID
+	FROM Users u
+	JOIN [SingleSignon].dbo.[Members] m ON m.ID = u.SingleSignonMemberID
+	JOIN [SingleSignon].dbo.[MemberRoles] mr ON mr.MemberID = m.ID
+	WHERE RoleName = 'SSC.NewsletterSubscriber'
+	AND (NOT(m.ApprovalDate IS NULL))
+	AND (mr.DeletedDate IS NULL)
+	AND UserID % 3 = 2
+END
+GO
