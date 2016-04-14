2562e50a5b3d83ffa89fa764fb7cff308546cea4
Bob <bob@bobslidell.com>
2016-04-14 10:37:38 +1000

    Added programmable objects

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/dbo/Stored Procedures/SetNewContentItemsForRelease.sql
@@ -0,0 +1,20 @@
+﻿IF OBJECT_ID('[dbo].[SetNewContentItemsForRelease]') IS NOT NULL
+	DROP PROCEDURE [dbo].[SetNewContentItemsForRelease];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+create procedure [dbo].[SetNewContentItemsForRelease]
+   @ContentItemID int
+ , @ReleaseDate datetime
+as
+
+insert ContentItemsScheduledRelease 
+ select @ContentItemID
+   , @ReleaseDate
+   , 0
+
+return
+GO
