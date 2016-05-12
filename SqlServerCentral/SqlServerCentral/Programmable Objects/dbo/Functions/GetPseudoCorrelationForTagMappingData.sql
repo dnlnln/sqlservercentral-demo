5da32b93d6b32a8e8898bf8bec084ec43e541831
Bob <bob@bobslidell.com>
2016-05-12 15:12:22 +0100

    Baseline database

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/dbo/Functions/GetPseudoCorrelationForTagMappingData.sql
@@ -0,0 +1,23 @@
+﻿IF OBJECT_ID('[dbo].[GetPseudoCorrelationForTagMappingData]') IS NOT NULL
+	DROP FUNCTION [dbo].[GetPseudoCorrelationForTagMappingData];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE FUNCTION [dbo].[GetPseudoCorrelationForTagMappingData] 
+(	
+	@tag1ID int, 
+	@tag2ID int
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+	SELECT SUM((tmmmd1.NumMappings - tmmmd1.MeanNumMappings) * (tmmmd2.NumMappings - tmmmd2.MeanNumMappings)) / STDEV(tmmmd2.NumMappings) as PseudoCorr
+	FROM GetTagMappingsWithMeanData(@tag1ID) tmmmd1
+	JOIN (SELECT * FROM GetTagMappingsWithMeanData(@tag2ID)) AS tmmmd2 ON [tmmmd2].ContentItemID=[tmmmd1].ContentItemID
+)
+
+GO
