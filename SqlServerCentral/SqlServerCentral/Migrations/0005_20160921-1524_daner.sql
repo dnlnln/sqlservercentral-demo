0f37e197c05589837adb079b503b5bc3e96a5138
Bob <bob@bobslidell.com>
2016-09-21 15:26:40 +0100

    Added index to correct the drift

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0005_20160921-1524_daner.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="c26025cb-f045-4331-a9fc-d79b4ce43ee0" />
+GO
+
+PRINT N'Creating index [IX_Answers] on [dbo].[Answers]'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.indexes WHERE name = N'IX_Answers' AND object_id = OBJECT_ID(N'[dbo].[Answers]'))
+CREATE NONCLUSTERED INDEX [IX_Answers] ON [dbo].[Answers] ([AnswerText])
+GO
