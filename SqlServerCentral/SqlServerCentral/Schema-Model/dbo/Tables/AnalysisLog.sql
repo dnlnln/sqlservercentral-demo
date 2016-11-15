0dac37ad168ad9c6d5c17ba143070241082a13bf
Bob <bob@bobslidell.com>
2016-11-16 08:43:53 +1100

    Initial import from production

---
+++ SqlServerCentral/SqlServerCentral/Schema-Model/dbo/Tables/AnalysisLog.sql
@@ -0,0 +1,12 @@
+CREATE TABLE [dbo].[AnalysisLog]
+(
+[AnalysisLogID] [int] NOT NULL IDENTITY(1, 1),
+[Time] [datetime] NOT NULL,
+[Data] [varchar] (50) NOT NULL CONSTRAINT [DF_AnalysisImageLog_Data] DEFAULT (''),
+[Count] [int] NOT NULL
+)
+GO
+ALTER TABLE [dbo].[AnalysisLog] ADD CONSTRAINT [PK__AnalysisImageLog__1CBC4616] PRIMARY KEY CLUSTERED  ([AnalysisLogID])
+GO
+CREATE NONCLUSTERED INDEX [_dta_index_AnalysisLog_8_562101043__K3_K2] ON [dbo].[AnalysisLog] ([Data], [Time])
+GO
