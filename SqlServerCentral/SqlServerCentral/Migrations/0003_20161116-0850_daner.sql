8390ee8bfd867810472fa0d876cc80b712da335f
Bob <bob@bobslidell.com>
2016-11-16 08:56:25 +1100

    Added column

---
+++ SqlServerCentral/SqlServerCentral/Migrations/0003_20161116-0850_daner.sql
@@ -0,0 +1,10 @@
+﻿-- <Migration ID="3a8d559d-9159-44f5-9ebd-ab7864ceb0db" />
+GO
+
+PRINT N'Altering [dbo].[EmailerConfiguration]'
+GO
+ALTER TABLE [dbo].[EmailerConfiguration] ADD
+[DateCreated] [datetime2] NOT NULL 
+CONSTRAINT [DF_EmailerConfiguration_DateCreated] 
+DEFAULT (sysdatetime())
+GO
--- SqlServerCentral/SqlServerCentral/Schema-Model/dbo/Tables/EmailerConfiguration.sql
+++ SqlServerCentral/SqlServerCentral/Schema-Model/dbo/Tables/EmailerConfiguration.sql
@@ -4,7 +4,8 @@ CREATE TABLE [dbo].[EmailerConfiguration]
 [Active] [bit] NOT NULL,
 [BatchSize] [int] NOT NULL,
 [Priority] [int] NOT NULL,
-[LastModifiedDate] [datetime2] NULL
+[LastModifiedDate] [datetime2] NULL,
+[DateCreated] [datetime2] NOT NULL CONSTRAINT [DF_EmailerConfiguration_DateCreated] DEFAULT (sysdatetime())
 )
 GO
 ALTER TABLE [dbo].[EmailerConfiguration] ADD CONSTRAINT [PK_EmailerConfiguration] PRIMARY KEY CLUSTERED  ([ComputerName])
