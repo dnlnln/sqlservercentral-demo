b4d22d6d109c3fb422704a2963d471355383a236
Jim <Jim@jimmidean.com>
2017-09-07 21:56:02 +1000

    Added Contacts.CreatedDate

---
+++ SqlServerCentral/SqlServerCentral/Migrations/1.1.0/0004_20170907-2155_daner.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="8b36c5e1-825a-421f-9a62-3e785dcb86de" />
+GO
+
+PRINT N'Altering [dbo].[Contacts]'
+GO
+ALTER TABLE [dbo].[Contacts] ADD
+[CreatedDate] [datetime2] NOT NULL CONSTRAINT [DF__Contacts__Create__15DA3E5D] DEFAULT (sysdatetime())
+GO
--- SqlServerCentral/SqlServerCentral/Schema-Model/dbo/Tables/Contacts.sql
+++ SqlServerCentral/SqlServerCentral/Schema-Model/dbo/Tables/Contacts.sql
@@ -8,7 +8,8 @@ CREATE TABLE [dbo].[Contacts]
 [Address2] [nvarchar] (128) NULL,
 [Address3] [nvarchar] (128) NULL,
 [JoiningDate] [datetime] NULL CONSTRAINT [DF__Contacts__Joinin__164452B1] DEFAULT (getdate()),
-[Email] [nvarchar] (256) NULL
+[Email] [nvarchar] (256) NULL,
+[CreatedDate] [datetime2] NOT NULL CONSTRAINT [DF__Contacts__Create__15DA3E5D] DEFAULT (sysdatetime())
 )
 GO
 ALTER TABLE [dbo].[Contacts] ADD CONSTRAINT [PK_Contacts] PRIMARY KEY CLUSTERED  ([ID])
