70c515a5b3a97093c0fc48992bd534948062271a
Jim <Jim@jimmidean.com>
2017-09-08 06:43:27 +1000

    Jim's change

--- SqlServerCentral/SqlServerCentral/Programmable Objects/dbo/Stored Procedures/AddStopWord.sql
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/dbo/Stored Procedures/AddStopWord.sql
@@ -6,7 +6,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-CREATE PROCEDURE [dbo].[AddStopWord] @Word NVARCHAR(50)
+CREATE PROCEDURE [dbo].[AddStopWord] @Word NVARCHAR(51) -- Jims change
 AS
 	BEGIN
 		INSERT INTO dbo.SearchAnalyzerConfig
