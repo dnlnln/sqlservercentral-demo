71cc516dfb466632178cf9afec550e143085429d
Jim <Jim@jimmidean.com>
2017-09-08 06:36:24 +1000

    Added NewCol

---
+++ SqlServerCentral/SqlServerCentral/Migrations/1.1.0/0004_20170908-0635_daner.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="df43ebf9-368d-4217-8cbb-a00c7f2c7858" />
+GO
+
+PRINT N'Altering [dbo].[EmailerConfiguration]'
+GO
+ALTER TABLE [dbo].[EmailerConfiguration] ADD
+[NewCol] [nchar] (10) NULL
+GO
