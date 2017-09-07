f0e6b45774b38f8f3670afde5556f0a5450f6126
Bob <bob@bobslidell.com>
2017-09-08 06:41:38 +1000

    Enable PO

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/dbo/Stored Procedures/ListSynonymGroups.sql
@@ -0,0 +1,20 @@
+﻿IF OBJECT_ID('[dbo].[ListSynonymGroups]') IS NOT NULL
+	DROP PROCEDURE [dbo].[ListSynonymGroups];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROCEDURE [dbo].[ListSynonymGroups]
+AS 
+    SELECT   Word , [Group]
+    FROM     dbo.SearchAnalyzerConfig
+    WHERE    [Group] != 0
+    ORDER BY [Group]
+
+GO
+GRANT EXECUTE ON  [dbo].[ListSynonymGroups] TO [ssc_searchindexer]
+GO
+GRANT EXECUTE ON  [dbo].[ListSynonymGroups] TO [ssc_webapplication]
+GO
