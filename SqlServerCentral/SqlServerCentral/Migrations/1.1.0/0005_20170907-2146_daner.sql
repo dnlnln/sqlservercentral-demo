69c7aad5cb551ebd59b9a01b7b44f45b4d043736
Jim <Jim@jimmidean.com>
2017-09-07 21:47:23 +1000

    Added Contacts.CreatedDate

---
+++ SqlServerCentral/SqlServerCentral/Migrations/1.1.0/0005_20170907-2146_daner.sql
@@ -0,0 +1,10 @@
+﻿-- <Migration ID="fc0768e9-500a-40b0-ace7-599c1268c08d" />
+GO
+
+PRINT N'Altering [dbo].[Contacts]...';
+
+
+GO
+ALTER TABLE [dbo].[Contacts]
+    ADD [CreatedDate] DATETIME2 (7) CONSTRAINT [DF_Contacts_CreatedDate] DEFAULT sysdatetime() NOT NULL;
+
