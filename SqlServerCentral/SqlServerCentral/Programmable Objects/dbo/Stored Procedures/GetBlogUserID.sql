18a9ed0a1077885404e9e20d68e199464a883e8f
Bob <bob@bobslidell.com>
2017-09-08 05:43:24 +1000

    Enable PO

---
+++ SqlServerCentral/SqlServerCentral/Programmable Objects/dbo/Stored Procedures/GetBlogUserID.sql
@@ -0,0 +1,17 @@
+﻿IF OBJECT_ID('[dbo].[GetBlogUserID]') IS NOT NULL
+	DROP PROCEDURE [dbo].[GetBlogUserID];
+
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_NULLS ON
+GO
+CREATE PROC [dbo].[GetBlogUserID]
+	@Email NVARCHAR(512)
+AS
+	SELECT UserID
+	FROM CommunityServer.dbo.cs_Users
+	WHERE Email = @Email
+GO
+GRANT EXECUTE ON  [dbo].[GetBlogUserID] TO [ssc_webapplication]
+GO
