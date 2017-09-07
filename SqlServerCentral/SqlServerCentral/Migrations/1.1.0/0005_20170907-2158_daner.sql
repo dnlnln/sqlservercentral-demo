d3c1cd25945d6f1acabd537deafbf1103bae5405
Bob <bob@bobslidell.com>
2017-09-07 21:59:06 +1000

    Updated data

---
+++ SqlServerCentral/SqlServerCentral/Migrations/1.1.0/0005_20170907-2158_daner.sql
@@ -0,0 +1,19 @@
+﻿-- <Migration ID="c7d5d3e2-47a4-427a-89ec-3f23b14412dd" />
+GO
+
+SET DATEFORMAT YMD;
+
+
+GO
+DECLARE @pv AS BINARY (16);
+
+
+GO
+PRINT (N'Update 1 row in [dbo].[EmailerConfiguration]');
+
+UPDATE [dbo].[EmailerConfiguration]
+SET    [Priority] = 3
+WHERE  [ComputerName] = N'252839-VMWEB4';
+
+
+GO
